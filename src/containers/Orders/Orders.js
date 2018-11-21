3d11b93c248933dd662e4589e78914e73251d53a
Riskey <riskey.it@gmail.com>
2018-11-21 14:41:15 +0530

    Finished 213

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { axOrders } from "../../axios";
 
-import Order from '../../components/Order/Order';
+import Order from "../../components/Order/Order";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        axOrders
+            .get("/orders.json")
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
+    }
 
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                    return (
+                        <Order
+                            key={order.id}
+                            ingredients={order.ingredients}
+                            price={order.price}
+                        />
+                    );
+                })}
             </div>
         );
     }
-
 }
 
 export default Orders;
